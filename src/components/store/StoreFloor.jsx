c02d821cc3262436725f05e4b5a6a3b35f556d16
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-06-19 16:56:56 +0700

    set color of tiles by for loop

--- src/components/store/StoreFloor.jsx
+++ src/components/store/StoreFloor.jsx
@@ -1,21 +1,16 @@
 export default function StoreFloor(){
-// const floors = []
-//     let num = 0
-//     for(let i =-4; i <=4; i ++){
-//         for(let y =-4; y <=4; y ++){
-//             floors.push({id: num+1, position: [i,y,0],  color: i %2==0 || y%2==0 ? '#ffffff' : '#eeeeee' }) 
-//         }
-//     }
+const floors = []
+    let num = 0
+    for(let i =-4; i <=4; i ++){
+        for(let y =-4; y <=4; y ++){
+            num++
+            floors.push({id: num, position: [i,y,0],  color: num%2==0  ? '#635d5d' : '#eeeeee' }) 
+        }
+    }
  
-const floors = [
-    { id: 1, position: [-2, 0, 0], color: 'hotpink' },
-    { id: 2, position: [0, 0, 0], color: 'royalblue' },
-    { id: 3, position: [2, 0, 0], color: 'orange' }
-  ];
-
     return floors.map(square => {
         return <mesh key={square.id} position={square.position}>
-            <planeGeometry args={[1.5, 1.5]} />
+            <planeGeometry args={[1, 1]} />
             <meshStandardMaterial color={square.color}  />
         </mesh>
     })

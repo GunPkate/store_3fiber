4c71e94f4d3f8d4a0d7ead6360ff30c4b4b8aa2c
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-06-20 08:07:17 +0700

    add single shelf model

--- src/components/MapLoader.jsx
+++ src/components/MapLoader.jsx
@@ -1,6 +1,8 @@
+import Shelf from "./facilities/Shelf";
 import StoreFloor from "./store/StoreFloor";
 import WallSection from "./store/WallSection";
 
+const shelfUnit = [[0,0,1] ,[0,0,3], [0,0,5]]
 export default function MapLoader(){
     return <>
     <group position={[0, 1, 0]} >
@@ -9,6 +11,10 @@ export default function MapLoader(){
 
     <group position={[0, 0, 0]} >
         <WallSection/>
+    </group>
+
+    <group position={[0, 0, 0]} >
+        <Shelf/>
     </group>
       </>
 }

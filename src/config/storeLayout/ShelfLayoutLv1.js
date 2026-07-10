5dd1a4b0d22211d1aee927809d86210f3a2c59a2
GunP <gundash1@gmail.com>
2026-07-10 21:04:40 +0700

    refactor cookies config and qty params in Customer class

---
+++ src/config/storeLayout/ShelfLayoutLv1.js
@@ -0,0 +1,15 @@
+export const SHELFLAYOUT = [
+  { label: 'Cookies', 
+    color:"#e07878",
+    details: // x =row size, z = stack
+      {
+        start: { x: -.35, y:.5, z: -.4},
+        size: { hw: .18, hh:.28, hd: .1},
+        distanceInRow: { x: -.25, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 6,
+    rowStack: 2,
+    shelfRow: 3
+  }, 
+]
--- src/config/storeProductList/lv1/StoreProductList.js
+++ src/config/storeProductList/lv1/StoreProductList.js
@@ -5,7 +5,7 @@ export function createStoreProductList() {
     { shelfIdx: 2, name: 'Candy', price: 1.0, maxQty: 20, qty: 20 },
     { shelfIdx: 3, name: 'Juice', price: 2.0, maxQty: 20, qty: 20 },
     { shelfIdx: 4, name: 'Chips', price: 2.5, maxQty: 20, qty: 20 },
-    { shelfIdx: 5, name: 'Cookies', price: 3.0, maxQty: 20, qty: 20 },
+    { shelfIdx: 5, name: 'Cookies', price: 3.0, maxQty: 36, qty: 36 },
     { shelfIdx: 6, name: 'Soap', price: 3.5, maxQty: 20, qty: 20 },
     { shelfIdx: 7, name: 'Shampoo', price: 4.0, maxQty: 20, qty: 20 },
     { shelfIdx: 8, name: 'Misc', price: 1.0, maxQty: 20, qty: 20 },

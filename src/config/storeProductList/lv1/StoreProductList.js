75c15722f837467e7bbcdfacf3aa4ce95b5a9061
GunP <gundash1@gmail.com>
2026-07-10 10:39:52 +0700

    shelf items refactor createItems to createStoreProductList

---
+++ src/config/storeProductList/lv1/StoreProductList.js
@@ -0,0 +1,13 @@
+export function createStoreProductList() {
+  return [
+    { shelfIdx: 0, name: 'Cola', price: 1.5, maxQty: 20, qty: 20 },
+    { shelfIdx: 1, name: 'Snacks', price: 2.5, maxQty: 20, qty: 20 },
+    { shelfIdx: 2, name: 'Candy', price: 1.0, maxQty: 20, qty: 20 },
+    { shelfIdx: 3, name: 'Juice', price: 2.0, maxQty: 20, qty: 20 },
+    { shelfIdx: 4, name: 'Chips', price: 2.5, maxQty: 20, qty: 20 },
+    { shelfIdx: 5, name: 'Cookies', price: 3.0, maxQty: 20, qty: 20 },
+    { shelfIdx: 6, name: 'Soap', price: 3.5, maxQty: 20, qty: 20 },
+    { shelfIdx: 7, name: 'Shampoo', price: 4.0, maxQty: 20, qty: 20 },
+    { shelfIdx: 8, name: 'Misc', price: 1.0, maxQty: 20, qty: 20 },
+  ];
+}

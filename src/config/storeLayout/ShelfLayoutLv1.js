57baec88f83302505abd09f0c25de3aa552a32f0
GunP <gundash1@gmail.com>
2026-07-11 22:18:58 +0700

    shelf items config all products

--- src/config/storeLayout/ShelfLayoutLv1.js
+++ src/config/storeLayout/ShelfLayoutLv1.js
@@ -1,4 +1,56 @@
 export const SHELFLAYOUT = [
+  { label: 'Cola', 
+    color:"#ded2ad",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: -4.95, y:.5, z: -3.3}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  },
+  { label: 'Snacks', 
+    color:"#e07878",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: -4.95, y:.5, z: -1.9},
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  },
+  { label: 'Juice', 
+    color:"#ded2ad",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: -.35, y:.5, z: -3.3}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  },
+  { label: 'Candy', 
+    color:"#e07878",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: -4.95, y:.5, z: -.4}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  },
   { label: 'Cookies', 
     color:"#e07878",
     details: // x =row size, z = stack (cookies x: -1, z: -0.5)
@@ -12,6 +64,19 @@ export const SHELFLAYOUT = [
     rowStack: 2,
     shelfRow: 3
   },
+  { label: 'Misc', 
+    color:"#78e082",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: 4.2, y:.5, z: -.4}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  },
   { label: 'Chips', 
     color:"#f9e247",
     details: // x =row size, z = stack (Chips x: -1, z: -2)
@@ -24,5 +89,31 @@ export const SHELFLAYOUT = [
     rowSize: 6,
     rowStack: 2,
     shelfRow: 3
+  }, 
+  { label: 'Shampoo', 
+    color:"#5f47f9",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: 4.2, y:.5, z: -1.9}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
+  }, 
+  { label: 'Soap', 
+    color:"#f947db",
+    details: // x =row size, z = stack (cookies x: -1, z: -0.5)
+      {
+        start: { x: 4.2, y:.5, z: -3.3}, //dif x .65 dif y .1
+        size: { hw: .10, hh:.28, hd: .1},
+        distanceInRow: { x: -.15, z: .2},
+        distanceBetweenShelf: { y: .75} 
+      },
+    rowSize: 10,
+    rowStack: 1,
+    shelfRow: 2
   }, 
 ]

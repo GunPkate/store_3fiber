090c9627d680ea7b64f019188309c8077d80bf10
GunP <gundash1@gmail.com>
2026-08-23 19:09:20 +0700

    fix lighting

--- src/components/store/building/store/CeilingLights.jsx
+++ src/components/store/building/store/CeilingLights.jsx
@@ -36,7 +36,7 @@ export function CeilingLights() {
               intensity={5}
               position={[lx, ly, lx]}
               penumbra={0.5}
-              angle={3}
+              angle={2}
               castShadow
               shadow-mapSize-width={2048}                                 
               shadow-mapSize-height={2048}
